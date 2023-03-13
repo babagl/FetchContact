98eafcc567da6ca81539c3f692d71e2225cb7999
Abdoulaye Aliou SALL <gallebaba71@gmail.com>
2023-03-13 14:38:03 +0000

    Initial Commit

---
+++ SearchList/SearchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListApp.swift
+//  SearchList
+//
+//  Created by Abdoulaye Aliou SALL on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
